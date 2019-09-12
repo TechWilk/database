261747fc989d5a23b81994545f92f35c151b5546
Contributor B <b@example.com>
2019-09-12 16:23:30 +0100

    Add MySqliDatabaseFromConnection

---
+++ src/MySqli/MySqliDatabaseFromConnection.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TechWilk\Database\MySqli;
+
+use TechWilk\Database\Exception\DatabaseException;
+
+class MySqliDatabaseFromConnection extends MySqliDatabase
+{
+    protected $mysqli;
+
+    public function __construct(
+        \mysqli $connection
+    ) {
+        $this->mysqli = $connection;
+
+        if ($this->mysqli->connect_errno) {
+            throw new DatabaseException(
+                'Failed to connect to MySQL: ('.$this->mysqli->connect_errno.') '.$this->mysqli->connect_error,
+                $this->mysqli->connect_errno
+            );
+        }
+
+        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    }
+
+    public function __destruct()
+    {
+        // We don't want to close this connection
+    }
+}
