7422d4fa3a8b20e0015d007a7606412882485c95
Christopher Wilkinson <c@wilk.tech>
2026-08-04 21:11:48 +0100

    Fix exception inheritance for BadFieldException

--- src/Exception/BadFieldException.php
+++ src/Exception/BadFieldException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace TechWilk\Database\Exception;
 
-class BadFieldException extends \Exception
+class BadFieldException extends DatabaseException
 {
     public function __construct($message, $code = 0, \Exception $previous = null)
     {
